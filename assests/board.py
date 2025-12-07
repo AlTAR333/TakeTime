0abf142e33d6b97c7c205d0e1849e23448419074
Armand <armand.vieau@epita.fr>
2025-12-07 19:51:11 +0100

    Add Remove and Clear cards

--- assests/board.py
+++ assests/board.py
@@ -23,13 +23,32 @@ class Board():
         for i in range(1, self.time+1):
             self.times[i] = Position(condition=self.timeConditions.get(i, ""))
 
-    def addCardtoPos(self):
-        pass
+    def addCardtoPos(self, position:int, card:tuple):
+        """
+        Add a card to a position
 
-    def removeCardfromPos(self):
-        pass
+        :param position: integer representing the position
+        :param card: tuple representing the added card
+        """
+        self.times[position].addCard(card)
+
+    def removeCardfromPos(self, position:int, instruction:str = "top"):
+        """
+        Add a card to a position
+
+        :param position: integer representing the position
+        :param instruction: string used to determine the method of removal (top if blank),
+                            "random" -> pop randomly
+                            "highest" -> pop highest card in hand
+                            "lowest" -> pop lowest card in hand
+                            "top" -> pop top card, most left card
+                            "bottom" -> pop bottom card, most right card
+        """
+        self.times[position].removeCard(instruction)
 
     def clear(self):
-        pass
-
-    
+        """
+        Clear all the positions
+        """
+        for position in self.times.values():
+            position.clear()

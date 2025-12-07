5d9b1e6764ec794ecd1e878d2ecf8802aff61081
Armand <armand.vieau@epita.fr>
2025-12-07 23:28:48 +0100

    Strategy 1 half implemented

--- game.py
+++ game.py
@@ -2,6 +2,7 @@ from assets.board import Board
 from assets.deck import Deck
 from assets.location import Location
 from assets.hand import Hand
+import math
 
 class Game():
     def __init__(self):
@@ -26,29 +27,42 @@ class Game():
         for player, hand in self.hands.items():
             print(f"{player} : {hand}")
 
-    def printBoard(self) -> None :
+    def printBoard(self) -> None:
         print("BOARD")
         print(f"{self.board}")
 
-    def preset1(self) -> None:
+    def changeCurrentPlayer(self) -> None:
+        self.playerPointer += 1
+        if self.playerPointer == len(self.playersNames):
+            self.playerPointer = 0
+        self.currentPlayer = self.playersNames[self.playerPointer]
+
+    def presets(self, preset:int = 1) -> None:
         """
         Basic game, 24 max per location
         """
-        self.deck = Deck(shuffled = True)
-        self.board = Board()
-        
+        if preset == "1":
+            self.deck = Deck(shuffled = True)
+            self.board = Board()
+        else :
+            raise "Preset not found"
 
-    def preset2(self):
-        """
-
-        """
-        pass
-
-    def preset3(self):
-        """
-        
-        """
-        pass
+    def strategies(self, strategy, turn):
+        # First strategy
+        if strategy == "1":
+            if turn == 0: # Player with the highest card has to start
+                max = 0
+                for player, hand in self.hands.items():
+                    handMax = hand.getMax()
+                    if handMax > max :
+                        max = handMax
+                        self.currentPlayer = player
+                self.playerPointer = self.playersNames.index(self.currentPlayer)
+            card = self.hands[self.currentPlayer].removeCard("highest") # Player plays his highest card
+            value = card[1]
+            self.board.addCardtoPos(math.ceil(value/2), card)
+        else :
+            raise "Strategy not found"
 
     def main(self) -> None:
         # Players
@@ -61,25 +75,39 @@ class Game():
                 self.playersNames.append(playerName)
         else :
             self.playersNames = [str(i) for i in range(1, self.players+1)]
+        
 
         # Preset load
         self.preset = input("What preset do you want to load : ")
-        if self.preset == "1":
-            self.preset1()
-        elif self.preset == "2":
-            self.preset2()
-        elif self.preset == "3":
-            self.preset3()
-        else :
-            raise "Preset not found"
+        self.presets(self.preset)
+        
+        self.strategy = input("What strategy do you want to use : ")
         
         # Start Game
-        self.dealHands()
-        print("=============================")
-        self.printHands()
-        print("")
-        self.printBoard()
+        win = 0
+        for round in range(3):
+            self.dealHands()
+            print("=============================")
+            for turn in range(12):
+                print("----------")
+                self.printHands()
+                self.printBoard()
+                self.strategies(self.strategy, turn)
+                print(f"Next player : {self.currentPlayer}")
+                self.changeCurrentPlayer()
+            print("-----FINAL-----")
+            self.printHands()
+            self.printBoard()
+            if self.board.win():
+                win += 1
+            self.deck.create()
+            self.deck.shuffle()
+            self.board.clear()
+            for hand in self.hands.values():
+                hand.clear()
 
+
+    
 
 game = Game()
 game.main()
